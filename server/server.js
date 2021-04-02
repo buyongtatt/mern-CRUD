3ac6bb7356d39a9c2de696a6ecab060885b7b766
021120 <yongtatt981213@hotmail.com>
2021-04-02 11:03:57 +0800

    Setting backend

---
+++ server/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(
+  express.json({
+    limit: "20mb",
+    extended: "true",
+  })
+);
+
+app.use(
+  express.urlencoded({
+    limit: "20mb",
+    extended: "true",
+  })
+);
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Connection is established and running on port: ${PORT}`);
+    })
+  )
+  .catch((err) => console.log(err.message));
+
+mongoose.set("useFindAndModify", false);
